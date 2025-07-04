7d0d2cea03c7a18f9b7763472524699c3c24f9f3
Rakshitha M <167229931+Rakshitha0102@users.noreply.github.com>
2025-07-05 01:14:56 +0530

    Update db.php

--- db.php
+++ db.php
@@ -3,12 +3,7 @@ $host = "sql12.freesqldatabase.com";
 $user = "sql12788372";              
 $password = "aTEa8yPPdA";              
 $database = "sql12788372"; 
-
-
 $conn = mysqli_connect($host, $user, $password, $database);
-
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-?>
-
