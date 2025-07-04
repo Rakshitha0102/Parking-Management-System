9ea88eecc71245964309fbae2ad5c106a21b8715
Rakshitha M <167229931+Rakshitha0102@users.noreply.github.com>
2025-07-05 01:08:29 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";       
-$user = "root";              
-$password = "";              
-$database = "parking_system"; 
+$host = "sql12.freesqldatabase.com";       
+$user = "sql12788372";              
+$password = "aTEa8yPPdA";              
+$database = "sql12788372"; 
 
 
 $conn = mysqli_connect($host, $user, $password, $database);
